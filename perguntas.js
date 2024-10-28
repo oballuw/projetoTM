e2b2be75226f4299cd62b5925f810a6ecba0a925
oballuw <132585218+oballuw@users.noreply.github.com>
2024-10-28 09:07:28 -0300

    ATUALIZACAO
    123321

---
+++ perguntas.js
@@ -0,0 +1,78 @@
+const perguntas = [[
+    ///Tênis de mesa
+    {
+        enunciado: "Você começa a partida com dois saques, qual é a sua estratégia?",
+        alternativas: [
+            {
+                texto: "Sacar a bola com backspin lateral curto.",
+                afirmacao: "O saque backspin e curto foi inesperado para o adversário e ele errou a recepção. Ponto para você!",
+                pontos: 1
+            },
+            {
+                texto: "Sacar a bola com topspin longo e rápido.",
+                afirmacao: "O saque longo e rápido fez o adversário voltar a bola com um drive potente, resultando em um ataque bem sucedido. Ponto para o adversário.",
+                pontos: 0
+            }
+        ]
+    },
+    {
+        enunciado: "O adversário fez um drive forte carregado de efeito no seu Backhand, O que você faz?",
+        alternativas: [
+            {
+                texto: "Devolvo a bola com um Overdrive",
+                afirmacao: "O overdrive pegou o adversário desprevinido e ele não consegue reagir. ponto para você!",
+                pontos: 1
+            },
+            {
+                texto: "Faço a devolução e fico na defesa",
+                afirmacao: "Ficar na defesa não foi uma boa ideia pois o adversário não parou de atacar. Ponto para o adversário.",
+                pontos: 0
+            }
+        ]
+    },
+    {
+        enunciado: "Seu seu adversário realiza um saque curto sem giro no seu Backhand. Qual é a sua jogada?",
+        alternativas: [
+            {
+                texto: "Devolvo com uma Chikita no Backhand.",
+                afirmacao: "A chikita foi bem sucedida, o adversário não conseguiu devolver e mandou a bola para fora. Ponto para você!",
+                pontos: 1
+            },
+            {
+                texto: "Devolvo com uma Cozinhada no Forehand.",
+                afirmacao: "A cozinhada resultou em uma alta para o adversário, resultando em um ataque rápido no seu forehand. Ponto para o adversário.",
+                pontos: 0
+            }
+        ]
+    },
+    {
+        enunciado: "Você está no meio de um ralli. O que você faz?",
+        alternativas: [
+            {
+                texto: "Manda um drive no Backhand do oponente.",
+                afirmacao: "O ataque resultou em um overdrive no seu backhand sem chance de devolução. Ponto para o adversário.",
+                pontos: 0
+            },
+            {
+                texto: "Manda um drive na direção do oponente.",
+                afirmacao: "O ataque foi bem sucedido e o adversário não conseguiu devolver. Ponto para você!",
+                pontos: 1
+            }
+        ]
+    },
+    {
+        enunciado: "Você está muito afastado da mesa e seu oponente está mandando uma sequência de ataques. Como você responde?",
+        alternativas: [
+            {
+                texto: "Fica na defesa até o erro do oponente.",
+                afirmacao: "O adversário mandou a bola no canto e você não conseguiu buscar. Ponto para o adversário.",
+                pontos: 0
+            },
+            {
+                texto: "Defende e vai chegando mais próximo da mesa, e na hora certa contra-ataca.",
+                afirmacao: "Você conseguiu chegar perto da mesa e acertou um giro lateral, resultando em um contra-ataque bem sucedido. Ponto para você!",
+                pontos: 1
+            }
+        ]
+    }]
+];
